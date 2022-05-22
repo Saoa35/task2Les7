c5b17ac5a75098539ff9c4fb281261f173e6df80
Saoa35 <saoamah@gmail.com>
2022-05-22 13:24:37 +0200

    create class toDoList

--- src/Components/ToDoList/ToDoList.js
+++ src/Components/ToDoList/ToDoList.js
@@ -1,3 +1,36 @@
 import React, { Component } from "react";
+import Header from './components/Header';
+import SubmitForm from "./components/SubmitForm";
 
-class ToDoList extends Component
+class ToDoList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: []
+        }
+
+        this.handleAddTodo = this.handleAddTodo.bind(this);
+    }
+
+    handleAddTodo(value) {
+
+        this.setState({
+            tasks: value
+        })
+    }
+
+    render() {
+
+        console.log(this.state.tasks);
+
+        return(
+            <div>
+                <Header tasksCount={this.state.tasks.length}/>
+                <SubmitForm onAddTodo={this.handleAddTodo}/>
+            </div>
+        )
+    }
+}
+
+export default ToDoList;
