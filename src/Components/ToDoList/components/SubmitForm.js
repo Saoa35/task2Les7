5613cdc86af8e704f865d79046ce8df7fe75cf5d
Saoa35 <saoamah@gmail.com>
2022-05-22 11:31:49 +0200

    create Submit form component

---
+++ src/Components/ToDoList/components/SubmitForm.js
@@ -0,0 +1,17 @@
+//SubmitForm
+
+import React, { Component } from "react";
+
+class Header extends Component {
+
+    render() {
+
+        return(
+            <header>
+                You have Todos 
+            </header>
+        )
+    }
+}
+
+export default Header;
