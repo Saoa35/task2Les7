ca2a2b432707362b8dcbe95869cd5fd1530647f5
Saoa35 <saoamah@gmail.com>
2022-05-22 10:10:50 +0200

    + MyComponents methods

--- src/Components/ComponentRender/ComponentRender.js
+++ src/Components/ComponentRender/ComponentRender.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
-export default class ComponentRender extends Component {
 
-    const MyComponents = {
-        Comp1: () => <div>render Component 1</div>,
-        Comp2: ({ name }) => {
-            return(
-                <div>
-                    <h3>Hi! My name is {name}</h3>
-                </div>
-            )
-        }
+const MyComponents = {
+    Comp1: () => <div>render Component 1</div>,
+    Comp2: ({ name }) => {
+        return(
+            <div>
+                <h3>Hi! My name is {name}</h3>
+            </div>
+        )
     }
+}
+
+export default class ComponentRender extends Component {
 
     render() {
 
@@ -20,6 +21,10 @@ export default class ComponentRender extends Component {
                 <h2>
                 ComponentRender
                 </h2>
+
+                <MyComponents.Comp1/>
+                <MyComponents.Comp2 name='Alexandr'/>
+
             </div>
         )
     }
