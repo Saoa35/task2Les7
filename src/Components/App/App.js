9e91af0c6e4c6f30354e34838d224356c613b6d5
Saoa35 <saoamah@gmail.com>
2022-05-22 10:11:54 +0200

    + ComponentRender

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
+import ComponentRender from '../ComponentRender/ComponentRender';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <ComponentRender/>
     </div>
   );
 }
