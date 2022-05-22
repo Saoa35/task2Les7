00a65b18175f4a289d16b1afe2a683c5605fb262
Saoa35 <saoamah@gmail.com>
2022-05-22 13:19:46 +0200

    create constructor @ methods

--- src/Components/ToDoList/components/SubmitForm.js
+++ src/Components/ToDoList/components/SubmitForm.js
@@ -1,14 +1,34 @@
-//SubmitForm
-
 import React, { Component } from "react";
 
 class SubmitForm extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleAddTodo = this.handleAddTodo.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({value: e.target.value})
+    }
+
+    handleAddTodo(e) {
+        e.preventDefault();
+        this.props.onAddTodo({
+            value: this.state.value
+        });
+    }
+
     render() {
 
         return(
-            <form>
-                <input type/>
+            <form onSubmit={this.handleAddTodo}>
+                <input type='text' value={this.state.value} onChange={this.handleChange}/>
+                <input type='submit' value='Submit'/>
             </form>
         )
     }
