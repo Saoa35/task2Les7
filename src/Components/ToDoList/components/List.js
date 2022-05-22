ce14304bfbf362f2e202e7238f2017f55d8460b0
Saoa35 <saoamah@gmail.com>
2022-05-22 15:53:42 +0200

    + import @ li

--- src/Components/ToDoList/components/List.js
+++ src/Components/ToDoList/components/List.js
@@ -1,15 +1,26 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
 
-class List extends Component {
+export default class List extends Component {
 
     render() {
 
         return(
             <ul>
-                 
+                {this.props.list.map(({id, value}) => { 
+                    return(
+                        <li key={id}>
+                            {value}
+
+                            <button style={{ cursor: "pointer" }} onClick={() => this.props.onDelete(id)}>
+                            <FontAwesomeIcon icon={faTrash}/>
+                            </button>
+                        </li>
+                    )
+                    }
+                )} 
             </ul>
         )
     }
 }
-
-export default List;
