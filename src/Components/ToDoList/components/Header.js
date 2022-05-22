b0d5fac0bb08b7fd4f04c0bdd37c81652fe89074
Saoa35 <saoamah@gmail.com>
2022-05-22 13:22:17 +0200

    + value in header

--- src/Components/ToDoList/components/Header.js
+++ src/Components/ToDoList/components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
 
         return(
             <header>
-                header 
+                You have {this.props.tasksCount} Todos 
             </header>
         )
     }
